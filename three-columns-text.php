7fb1be1dd0e23120301bc42ad5c2328b351f37b8
rayflores <rayflores88@gmail.com>
2024-08-15 20:04:58 -0400

    About Contact block paragraph

---
+++ three-columns-text.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Title: About Contact
+ * Slug: hpu24/about-contact
+ * Description: A block pattern with a heading and a paragraph in one column, contact information in the second column, and a faculty contact details in the third column.
+ * Categories: hpu24/card
+ * Keywords: about, faculty, profile, paragraph, contact, information
+ * Viewport Width: 800
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xx-large","bottom":"var:preset|spacing|xx-large","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-color has-primary-background-color has-text-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--xx-large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--xx-large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|x-large","left":"var:preset|spacing|x-large"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"45%","style":{"spacing":{"blockGap":"var:preset|spacing|small"}}} -->
+<div class="wp-block-column" style="flex-basis:45%"><!-- wp:heading {"textAlign":"center","level":3,"fontSize":"x-large"} -->
+<h3 class="wp-block-heading has-text-align-center has-x-large-font-size">100%</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"base","fontSize":"medium"} -->
+<p class="has-text-align-center has-base-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:600">Performance</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","textColor":"primary-accent","fontSize":"small"} -->
+<p class="has-text-align-center has-primary-accent-color has-text-color has-small-font-size">Blazing-fast performance out of the box. No caching plugins needed.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","level":3,"fontSize":"x-large"} -->
+<h3 class="wp-block-heading has-text-align-center has-x-large-font-size">100%</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"base","fontSize":"medium"} -->
+<p class="has-text-align-center has-base-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:600">SEO Boost</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","textColor":"primary-accent","fontSize":"small"} -->
+<p class="has-text-align-center has-primary-accent-color has-text-color has-small-font-size">Built with SEO best practices in mind to help you rank in search engines.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","level":3,"fontSize":"x-large"} -->
+<h3 class="wp-block-heading has-text-align-center has-x-large-font-size">100%</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"base","fontSize":"medium"} -->
+<p class="has-text-align-center has-base-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:600">Accessibility</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","textColor":"primary-accent","fontSize":"small"} -->
+<p class="has-text-align-center has-primary-accent-color has-text-color has-small-font-size">Accessibility compliant to ensure equal access to content and functionality.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
